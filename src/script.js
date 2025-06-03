a6e126cf6d988524beb8defe152d837d75a9ef44
unknown <sweetali0520@gmail.com>
2025-06-03 11:31:09 +0600

    fix:nav

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
 const toggles = document.querySelectorAll(".accordion-toggle");
+const menuBtn = document.getElementById("menuToggle");
+const mobileMenu = document.getElementById("mobileMenu");
+const menuClose = document.getElementById("menuClose");
 
+// Accordian
 toggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
     const content = toggle.nextElementSibling;
@@ -14,4 +18,14 @@ toggles.forEach((toggle) => {
     // Toggle current
     content.classList.toggle("hidden");
   });
+});
+
+// show mobile menu
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("translate-x-60");
+});
+
+// hide mobile menu
+menuClose.addEventListener("click", () => {
+  mobileMenu.classList.add("translate-x-60");
 });
